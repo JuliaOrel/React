cf4300f11c0502fcdbd5ca2dc0dc7ed6b3073dcc
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-05-22 14:04:40 +0300

    HW_2 components_2

--- vue-helloworld/src/App.vue
+++ vue-helloworld/src/App.vue
@@ -4,13 +4,18 @@
 import Mynavbar from "@/components/adminlte/Mynavbar.vue";
 import MyCard from "@/components/adminlte/MyCard.vue";
 import Mysmallbox from "@/components/adminlte/Mysmallbox.vue";
+import MyTimeline from "@/components/adminlte/MyTimeline.vue";
+
 </script>
 
 <template>
 
-    <Mynavbar></Mynavbar>
-    <MyCard></MyCard>
-    <Mysmallbox></Mysmallbox>
+    <div class="wrapper">
+           <Mynavbar></Mynavbar>
+           <MyCard></MyCard>
+           <Mysmallbox></Mysmallbox>
+        <MyTimeline></MyTimeline>
+    </div>
 
 </template>
 
