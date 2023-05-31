f1f80631c1d7023726ec0d5347bbd9dfc69daa86
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-01 00:28:55 +0300

    design template_home

--- HW_3helloApi/front_api/src/views/HomeView.vue
+++ HW_3helloApi/front_api/src/views/HomeView.vue
@@ -2,14 +2,16 @@
 import {onMounted} from "vue";
 import {toast} from "vue3-toastify";
 import HoroscopeApi from "@/components/HoroscopeApi/HoroscopeApi.vue";
+import SiteBreadcrumbs from "@/layout/SiteBreadcrumbs.vue";
 
-
+const pageTitle="Home page"
 onMounted(() =>{
     toast.info("Open Home Page")
 })
 </script>
 
 <template>
+    <SiteBreadcrumbs :pageTitle=pageTitle></SiteBreadcrumbs>
     <section class="inner-page">
         <div class="container">
             <h2>
