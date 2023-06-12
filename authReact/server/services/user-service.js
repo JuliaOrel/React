1e68a20ce07df9a2da24bd51c5f47a146b243a24
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-12 22:12:53 +0300

    Authorization

---
+++ authReact/server/services/user-service.js
@@ -0,0 +1,20 @@
+const UserModel=require('../models/user-model');
+const bcrypt=require('bcrypt');
+const uuid=require('uuid');
+const mailService=require('./mail-service');
+const tokenService=require('../services/token-service')
+class UserService{
+ async registration(email, password){
+     const candidate=await UserModel.findOne({email})
+     if(candidate){
+         throw new Error(`User with such email ${email} already exists`)
+     }
+     const hashPassword=await bcrypt.hash(password, 3);
+     const activationLink=uuid.v4();
+     const user=await UserModel.create({email, password: hashPassword, activationLink})
+     await mailService.sendActivationMail(email, activationLink);
+     const tokens=tokenService.generateTokens()
+ }
+}
+
+module.exports=new UserService();
