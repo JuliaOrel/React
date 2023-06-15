de4ec0dea193a209098d224e088f1e44541317eb
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-16 01:14:58 +0300

    tokens

--- HW_3helloApi/front_api/src/stores/auth/auth.js
+++ HW_3helloApi/front_api/src/stores/auth/auth.js
@@ -2,16 +2,20 @@ import {defineStore} from "pinia";
 import {toast} from "vue3-toastify";
 import MyLog from "@/services/MyLog";
 import myAxios from "@/services/myAxios";
+import myLocalStorage from "@/services/myLocalStorage";
 
 export const useAuthStore= defineStore('auth', {
     state: () =>({
     isPreload: false,
         isError: false,
         error: {},
-        isLogin: false
+        isLogin: myLocalStorage.getItem('isLogin') || null,
+        user: myLocalStorage.getItem('user') || null,
+        token: myLocalStorage.getItem('token') || null
     }),
     actions: {
     doRegister(newUser){
+        this.isPreload=true
         // toast.info(newUser.name)
         // console.log(newUser.name)
         myAxios('/api/auth/register/', {
@@ -22,9 +26,20 @@ export const useAuthStore= defineStore('auth', {
             data: newUser
         })
             .then(res=> {
-                MyLog(res)
+                this.isPreload=false
+                this.user=res.user
+                this.token=res.token
+                this.isLogin=true
+                myLocalStorage.setItem('user', res.user)
+                myLocalStorage.setItem('token', res.token)
+                myLocalStorage.setItem('isLogin', true)
+                res.redirect('/')
+                //MyLog(res)
             }). catch(err => {
                 MyLog(err)
+            this.isPreload=false
+            this.isError=true
+            this.error=err
                 toast.error(err.message)
         })
     }
