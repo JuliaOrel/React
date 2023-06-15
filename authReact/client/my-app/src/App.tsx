dbb6970a26eea24b5e49efb4f0eb2262467ea940
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-15 19:01:08 +0300

    AuthorizationReact

--- authReact/client/my-app/src/App.tsx
+++ authReact/client/my-app/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC, useContext, useEffect} from 'react';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {observer} from "mobx-react-lite";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const {store} = useContext(Context)
+    useEffect(()=>{
+        if(localStorage.getItem('token')){
+            store.checkAuth()
+        }
+    }, [])
+    if(store.isLoading){
+        return <div>Loading...</div>
+    }
+    if(!store.isAuth){
+        return (<LoginForm/>)
+    }
+    return (
+        <div>
+            <h1>{store.isAuth ? `User is authorized ${store.user.email}`:'You need to authorize'}</h1>
+          <button onClick={()=> store.logout()}>Logout</button>
+            <div>
+                <button>Get users</button>
+            </div>
+        </div>
+    );
+};
 
-export default App;
+export default observer(App);
