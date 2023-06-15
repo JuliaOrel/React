d3c094de86b3366182092c9c6896ab80ab7ff969
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-15 20:10:21 +0300

    AuthorizationReact

--- authReact/client/my-app/src/App.tsx
+++ authReact/client/my-app/src/App.tsx
@@ -1,28 +1,45 @@
-import React, {FC, useContext, useEffect} from 'react';
+import React, {FC, useContext, useEffect, useState} from 'react';
 import LoginForm from "./components/LoginForm";
 import {Context} from "./index";
 import {observer} from "mobx-react-lite";
+import {IUser} from "../models/response/IUser";
+import UserService from "./services/UserService";
 
 const App: FC = () => {
-    const {store} = useContext(Context)
+    const {store} = useContext(Context);
+    const [users, setUsers]=useState<IUser[]>([]);
     useEffect(()=>{
         if(localStorage.getItem('token')){
             store.checkAuth()
         }
     }, [])
+
+    async function getUsers(){
+        try {
+            const response = await UserService.fetchUsers();
+            setUsers(response.data);
+        }catch (e) {
+            console.log(e)
+        }
+    }
     if(store.isLoading){
         return <div>Loading...</div>
     }
     if(!store.isAuth){
-        return (<LoginForm/>)
+        return <div>
+            <LoginForm/>
+            <button onClick={getUsers}>Get users</button>
+        </div>
     }
     return (
         <div>
             <h1>{store.isAuth ? `User is authorized ${store.user.email}`:'You need to authorize'}</h1>
           <button onClick={()=> store.logout()}>Logout</button>
             <div>
-                <button>Get users</button>
+                <button onClick={getUsers}>Get users</button>
             </div>
+            {users.map(user =>
+                <div key={user.email}>{user.email}</div>)}
         </div>
     );
 };
