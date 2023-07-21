4aacb380acfbe821924334a6424d831ed7f35657
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-07-21 20:00:01 +0300

    Laravel light

---
+++ laravel_services/front/public/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import {onMounted} from "vue";
+import {useSocketMainStore} from "@/stores/sockets/socket.main";
+import RegisterForm from "@/components/forms/auth/RegisterForm.vue";
+
+onMounted( () => {
+  const socketsMainStore = useSocketMainStore()
+  socketsMainStore.connect()
+})
+</script>
+
+<template>
+  <header> Public 1 </header>
+  <main id="main">
+    <RegisterForm></RegisterForm>
+  </main>
+  <footer> &copy; ItStep 2023 </footer>
+
+</template>
+
+
+<style scoped>
+</style>
---
+++ laravel_services/storages/minio/app-init.sh
@@ -0,0 +1,19 @@
+##!/bin/bash
+#
+## Создание хранилища
+mc alias set storage http://localhost:9000 root password
+mc config host add storage http://localhost:9000 root password
+
+
+mc config host add storage http://localhost:9000 TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
+#
+## Создание нового пользователя
+mc admin user add storage webuser kfFh3J2gkD56Hl3f4d7
+#
+## Добавление пользователю политики доступа
+mc admin policy atach storage readwrite user=webuser
+mc admin policy create readwrite user=webuser
+mc admin user key add storage webuser TGtZfe96wQPIx0dY kD9IjnOpgdG0l33JSMGhLWmPacNIxw7y
+#
+##Запуск сервера
+##mc server --console-address :9009 /data
