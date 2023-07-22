6f85d6d9b22103b16141d0f6b646b999f281c57e
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-07-22 17:40:40 +0300

    Laravel sockets

--- laravel_services/api/laravel/core/app/Http/Controllers/SocketController.php
+++ laravel_services/api/laravel/core/app/Http/Controllers/SocketController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\Interfaces\ISocket;
 use Illuminate\Support\Facades\Log;
 use SocketIO;
 
 class SocketController extends Controller
 {
+    public function __construct(protected ISocket $service)
+    {
+    }
+
     function index(){
         return view('Socket');
     }
@@ -15,10 +20,11 @@ class SocketController extends Controller
         Log::debug($d);
 
         //Из этого места в коде мне нужно отправить сообщение на фронт
-        $redis = new \Redis(); // Using the Redis extension provided client
-        $redis->connect('redis.socket', '6379');
-        $emitter = new SocketIO\Emitter($redis);
-        $emitter->broadcast->emit('controller_event', $d);
+//        $redis = new \Redis(); // Using the Redis extension provided client
+//        $redis->connect('redis.socket', '6379');
+//        $emitter = new SocketIO\Emitter($redis);
+//        $emitter->broadcast->emit('controller_event', $d);
+        $this->service->emit('controller_event', $d);
 
         return $d;
     }
