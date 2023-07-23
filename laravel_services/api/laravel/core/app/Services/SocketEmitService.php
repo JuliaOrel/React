bc2c0641e63ad8682c2909745b292ef00332e2b0
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-07-23 18:28:16 +0300

    Laravel sockets

--- laravel_services/api/laravel/core/app/Services/SocketEmitService.php
+++ laravel_services/api/laravel/core/app/Services/SocketEmitService.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Services;
-
+use Illuminate\Support\Facades\Redis;
 use App\Services\Interfaces\ISocket;
 use SocketIO;
 class SocketEmitService implements ISocket
@@ -10,8 +10,8 @@ class SocketEmitService implements ISocket
     private $emitter;
     public function __construct()
     {
-        $this->redis=new \Redis();
-        $this->redis->connect('redis.socket', '6379');
+        $this->redis=new \Redis();//::connection('default');
+        $this->redis->connect(env('REDIS_SOCKET_HOST', 'redis.socket'), env('REDIS_SOCKET_PORT', 6379));
         $this->emitter = new SocketIO\Emitter($this->redis);
     }
 
