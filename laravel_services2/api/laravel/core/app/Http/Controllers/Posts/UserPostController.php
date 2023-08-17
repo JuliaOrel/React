9c8f1163b4529944294ecd6db39e06eee2d5c224
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-08-17 21:40:01 +0300

    laravel posts by id

--- laravel_services2/api/laravel/core/app/Http/Controllers/Posts/UserPostController.php
+++ laravel_services2/api/laravel/core/app/Http/Controllers/Posts/UserPostController.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Str;
 
 class UserPostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      */
